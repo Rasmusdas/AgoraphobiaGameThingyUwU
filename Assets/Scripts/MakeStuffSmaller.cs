0809605c8d995ce8440385dbfb4546b6ed6691d3
Rasmusdas <rasmusmkn@gmail.com>
2019-11-13 15:58:10 +0100

    Added Project

---
+++ Assets/Scripts/MakeStuffSmaller.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MakeStuffSmaller : MonoBehaviour
+{
+    [SerializeField]
+    private float scaleModifier;
+
+    [SerializeField]
+    private Transform target;
+
+    [SerializeField]
+    private Transform point;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float dist = Vector3.Distance(point.position, target.position)*scaleModifier;
+        
+        
+        target.localScale = (Vector3.one * dist);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float movementSpeed;
+    void FixedUpdate()
+    {
+        float x = Input.GetAxis("Horizontal")*movementSpeed * Time.deltaTime;
+        float y = Input.GetAxis("Vertical") * movementSpeed*Time.deltaTime;
+
+        transform.Translate(x,0,y);
+    }
+}
