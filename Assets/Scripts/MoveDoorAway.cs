00d171cef6de09e16deae4e00da612067619d9c8
rasmusdas <rasmusmkn@gmail.com>
2019-11-27 16:31:01 +0100

    Added Post Processing to FuzzyDoor Prototype

---
+++ Assets/Scripts/MoveDoorAway.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveDoorAway : MonoBehaviour
+{
+    [SerializeField]
+    private Transform player;
+
+    [SerializeField]
+    private Transform door;
+
+    [SerializeField]
+    private float distance;
+    void Update()
+    {
+        if (Vector3.Distance(player.position,door.position) < distance)
+        {
+            StartCoroutine(MovingDoorAway(5));
+        }
+    }
+
+    IEnumerator MovingDoorAway(int distance)
+    {
+        if(distance <= 1000)
+        {
+            transform.localScale = new Vector3(1,1, transform.localScale.z)*(1+(2f/distance));
+            yield return new WaitForSeconds(0f);
+            StartCoroutine(MovingDoorAway(distance+8));
+        }
+    }
+}
