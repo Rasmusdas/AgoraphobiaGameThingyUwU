dcb1fcb9344610e8f461987c465addd26f6e76f8
RedYodaDK <stigfrederiksen32@gmail.com>
2019-12-04 15:59:21 +0100

    Walking sound added

--- Assets/Scripts/AudioController.cs
+++ Assets/Scripts/AudioController.cs
@@ -5,26 +5,14 @@ using UnityEngine.Audio;
 
 public class AudioController : MonoBehaviour
 {
-    // Dictionary<string,AudioSource> audioSouces = new Dictionary<string, AudioSource>();
-    public List<AudioSource> audioSouces;
+    public AudioSource audioSouce;
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetButtonDown("Fire1"))
         {
-            
-        }
-    }
-
-    public void PlaySoundEffect(string tag)
-    {
-        for (int i = 0; i < audioSouces.Count; i++)
-        {
-            if (audioSouces[i].CompareTag(tag))
-            {
-                audioSouces[i].Play();
-            }
+            audioSouce.Play();
         }
     }
 }
