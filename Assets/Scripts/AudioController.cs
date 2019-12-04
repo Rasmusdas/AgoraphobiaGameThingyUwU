ea59a32f3b46f531dca894ac5627166a00bffc5e
RedYodaDK <stigfrederiksen32@gmail.com>
2019-12-04 15:12:49 +0100

    Audio Stuff Added

--- Assets/Scripts/AudioController.cs
+++ Assets/Scripts/AudioController.cs
@@ -1,18 +1,30 @@
-﻿using System.Collections;
+﻿using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
 
 public class AudioController : MonoBehaviour
 {
-    public AudioSource testSound;
+    // Dictionary<string,AudioSource> audioSouces = new Dictionary<string, AudioSource>();
+    public List<AudioSource> audioSouces;
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetButtonDown("Fire1"))
         {
-            testSound.Play();
+            
+        }
+    }
+
+    public void PlaySoundEffect(string tag)
+    {
+        for (int i = 0; i < audioSouces.Count; i++)
+        {
+            if (audioSouces[i].CompareTag(tag))
+            {
+                audioSouces[i].Play();
+            }
         }
     }
 }
