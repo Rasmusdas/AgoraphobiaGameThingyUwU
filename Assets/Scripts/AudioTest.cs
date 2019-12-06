d201a695089323880812260543b579c28c9566ff
RedYodaDK <stigfrederiksen32@gmail.com>
2019-12-06 09:22:13 +0100

    Audio Testing and clicking sound added

---
+++ Assets/Scripts/AudioTest.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioTest : MonoBehaviour
+{
+    public AudioSource testSouce;
+    public float whait;
+    public float whait2;
+    public bool audio = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        testSouce = transform.GetComponent<AudioSource>();
+        StartCoroutine(AudioCoroutine());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire2"))
+        {
+            testSouce.Play();
+        }
+    }
+
+    void FixedUpdate()
+    {/*
+        if (audio)
+        {
+            new WaitForSeconds(whait);
+            testSouce.Play();
+        }*/
+    }
+
+    IEnumerator AudioCoroutine()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(Random.Range(whait,whait2));
+            if (audio)
+            {
+                testSouce.Play();
+            }
+        }
+    }
+}
