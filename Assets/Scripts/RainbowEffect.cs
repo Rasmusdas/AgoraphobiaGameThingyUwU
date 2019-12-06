248ae3048a6b2035f72f4e93cbe2462124e6c912
rasmusdas <rasmusmkn@gmail.com>
2019-12-06 08:41:38 +0100

    Added glow shader (needs testing problems with sharp corners)

---
+++ Assets/Scripts/RainbowEffect.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RainbowEffect : MonoBehaviour
+{
+    int r = 255;
+    int g = 0;
+    int b = 0;
+
+    Renderer rend;
+
+    [ColorUsageAttribute(true, true)]
+    Color color = new Color();
+
+    private void Start()
+    {
+        rend = GetComponent<Renderer>();
+        StartCoroutine(StartRainbow());
+    }
+
+    IEnumerator StartRainbow()
+    {
+        for (int i = 0; i <= 255; i += 10)
+        {
+            print("g++");
+            yield return new WaitForSeconds(0.001f);
+            g = i;
+            rend.material.SetColor("_Color", new Color(r, g, b)/ 64);
+        }
+        for (int i = 255; i >= 0; i -= 10)
+        {
+            print("r--");
+            yield return new WaitForSeconds(0.001f);
+            r = i;
+            rend.material.SetColor("_Color", new Color(r, g, b) / 64);
+        }
+        for (int i = 0; i <= 255; i += 10)
+        {
+            print("b++");
+            yield return new WaitForSeconds(0.001f);
+            b = i;
+            rend.material.SetColor("_Color", new Color(r, g, b) / 64);
+        }
+        for (int i = 255; i >= 0; i -= 10)
+        {
+            print("g--");
+            yield return new WaitForSeconds(0.001f);
+            g = i;
+            rend.material.SetColor("_Color", new Color(r, g, b) / 64);
+        }
+        for (int i = 0; i <= 255; i += 10)
+        {
+            print("r++");
+            yield return new WaitForSeconds(0.001f);
+            r = i;
+            rend.material.SetColor("_Color", new Color(r, g, b) / 64);
+        }
+        for (int i = 255; i >= 0; i -= 10)
+        {
+            print("b--");
+            yield return new WaitForSeconds(0.001f);
+            b = i;
+            rend.material.SetColor("_Color", new Color(r, g, b) / 64);
+        }
+        StartCoroutine(StartRainbow()); 
+    }
+}
