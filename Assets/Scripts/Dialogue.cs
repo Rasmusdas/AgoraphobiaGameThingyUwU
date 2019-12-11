e2e8dfc83c27804a3a734173d930f1045fa7e738
rasmusdas <rasmusmkn@gmail.com>
2019-12-11 13:33:51 +0100

    Added dialogue prototype

---
+++ Assets/Scripts/Dialogue.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Dialogue : MonoBehaviour
+{
+    public List<TextMessageConversation> conversations = new List<TextMessageConversation>();
+    public List<GameObject> textBoxes = new List<GameObject>();
+    public GameObject textBoxBase;
+    public Transform textParent;
+    public Transform textStart;
+
+    private bool ready;
+    private float timePerChar = 0.1f;
+    private bool clear;
+
+    private void Start()
+    {
+        GameObject textBox = Instantiate(textBoxBase,textStart.position,textBoxBase.transform.rotation,textParent);
+        foreach (GameObject g in textBoxes)
+        {
+            g.transform.Translate(0, 100, 0);
+        }
+        textBoxes.Add(textBox);
+        StartCoroutine(WriteLetter("owo what's this uwu", textBox));
+    }
+
+    public IEnumerator WriteLetter(string s, GameObject textBox)
+    {
+        Text text = textBox.transform.GetChild(0).GetComponent<Text>();
+        ready = false;
+        int i = 0;
+        while (i < s.Length && !clear)
+        {
+            yield return new WaitForSeconds(timePerChar);
+            text.text += s[i];
+            i++;
+        }
+        text.text += "\n";
+        if (clear)
+        {
+            text.text = "";
+            clear = false;
+        }
+        yield return new WaitForSeconds(timePerChar * 10);
+        ready = true;
+    }
+}
+
+[System.Serializable]
+public class TextMessageConversation
+{
+    public List<string> messages;
+}
