fbd583ba3dd5a2335183a7273fb16540e2500984
Rasmusdas <rasmusmkn@gmail.com>
2020-01-09 20:22:23 +0100

    no message

--- Assets/Scripts/Dresser.cs
+++ Assets/Scripts/Dresser.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class Dresser : Interactable
 {
+    public AudioSource closeSound;
     public override void Interact()
     {
         PlayerStress.minStressValue -= 25;
         PlayerStress.maxStressValue -= 20;
         interacted = true;
+        StartCoroutine(OpenClose());
+        
+    }
+    IEnumerator OpenClose()
+    {
+        sound.Play();
+        yield return new WaitForSeconds(2f);
+        closeSound.Play();
     }
 }
