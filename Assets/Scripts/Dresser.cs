b7d99fd171b769656087add9a877f66f31b371d7
Rasmusdas <rasmusmkn@gmail.com>
2020-01-09 20:30:17 +0100

    My house scene is better frick you

--- Assets/Scripts/Dresser.cs
+++ Assets/Scripts/Dresser.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class Dresser : Interactable
 {
+    public AudioSource closeSound;
     public override void Interact()
     {
         PlayerStress.minStressValue -= 25;
         PlayerStress.maxStressValue -= 20;
         interacted = true;
+        StartCoroutine(CloseDresser());
+    }
+
+    IEnumerator CloseDresser()
+    {
+        sound.Play();
+        yield return new WaitForSeconds(2f);
+        closeSound.Play();
     }
 }
