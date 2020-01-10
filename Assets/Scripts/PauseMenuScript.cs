6a8d2f1618a817fc97a90a7fb6db19299de51188
RedYodaDK <stigfrederiksen32@gmail.com>
2020-01-10 09:30:25 +0100

    Pause Menu Stuff

---
+++ Assets/Scripts/PauseMenuScript.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuScript : MonoBehaviour
+{
+    public GameObject canvas;
+    bool active;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        canvas = transform.GetChild(0).gameObject;
+        active = canvas.activeInHierarchy;
+        Time.timeScale = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            TogglePauseGame();
+        }
+    }
+
+    void TogglePauseGame()
+    {
+        active = !active;
+        canvas.SetActive(active);
+        if (active)
+        {
+            Time.timeScale = 0;
+        }
+        else
+        {
+            Time.timeScale = 1;
+        }
+
+    }
+
+    public void OpenMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void ResumeGame()
+    {
+        TogglePauseGame();
+    }
+}
